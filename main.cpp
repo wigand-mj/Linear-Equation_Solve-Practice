b41fb5ed9bf1adc98f2ca5ac17d115e1fb55ad08
maik-jw <->
2019-07-29 15:49:52 +0200

    now initialized random vectors a and b on startup

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstdlib>
 #include "lineq.h"
 #include "user_interface.h"
 
@@ -12,12 +13,33 @@ les machine(1,1);
 
 //declaring (and initializing states for state machine)
 string STATE = "INIT"; string nSTATE;
+
+vector<double> b;
+vector < vector<double>> a(1); //2-D A matrix v[i][j] where i is the column and j is the row
+
 //Function declarations
+
+void set_fixed_vector(vector <vector<double>> &a, vector<double> &b){
+    //lets take vars and eqs 2
+    int vars = 2; int eqs = 2;
+    for (int i=0; i<vars; i++){
+        for (int j=0; j<eqs; j++){
+            a[i][j] = (rand() % 10) + 1;
+        }
+    }
+    for (int j=0; j<eqs; j++){
+        b[j] = (rand() % 10) + 1;
+    }
+
+
+}
+
 string user_input();
 void draw_matrix(vector<int> row,vector<int> column , int size);
 
 
 int main(){
+set_fixed_vector(a,b);
 vector<int> row;
 vector<int> column;
 row[0]=1;
