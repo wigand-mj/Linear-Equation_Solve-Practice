08442b9299bc3b4940092dcca3454117ec90ca01
maik-jw <->
2019-08-01 18:20:29 +0200

    added difficulty variable and made operations() compatible with it

--- main.cpp
+++ main.cpp
@@ -8,7 +8,11 @@
 using namespace std;
 using std::string;
 
+// difficulty level
 
+short difficulty;
+
+// system dimensions
 
 int vars = 1; int eqs = 1;
 
@@ -60,38 +64,30 @@ template<typename Container>
         }   catch (const std::exception &e) {std::cout << "Error. " << std::endl; std::cout << "Something was wrong with your input. Try again" << std::endl; STATE = "TRANS";}
     }
 
+
+/*
+if (difficulty>0){
+    if (difficulty<2){
+        // =1
+    }
+    else { // =2 }
+}
+else  {  // = }
+*/
+
+
+
+
 // asking for input and working with gien user input
 void operation(){
     std::cout << "Which operation do you want to perform?" << std::endl;
-    std::cout << "(s)wap - (m)ultiplicate - (d)ivide - (a)dd - (su)btract   | [(e)xit]" << std::endl;
+    std::cout << "(s)wap" << ((difficulty >= 1) ? (" - (a)dd - (su)btract") : ("")) << ((difficulty == 2) ? (" - (m)ultiplicate - (d)ivide") : ("")) << "   | [(e)xit]" << std::endl;
     std::string input;
     std::string op;
     try { std::cin >> input; }
     catch (const std::exception &e) {std::cout << "Error. " << std::endl; std::cout << "Something was wrong with your input. Try again" << std::endl; nSTATE = "OPERATIONS"; STATE = "TRANS";}
     
-
-    if (input == "a"){
-        
-        std::cout << "adding ROW_A to ROW_B [Input: ROWB,ROWA]" << std::endl;
-        std::cin >> op;
-        std::vector<double> order; // TODO change so that is does not have to be re-created all the time
-        string_parse(",", op, order);
-
-        machine.rows_add(a, order); machine.rows_add(b, order);
-        
-        nSTATE = "DRAW";
-        STATE = "TRANS";
-    } else if (input == "m"){
-        std::cout << "multiplicating ROW with factor [Input: ROW,FACTOR]" << std::endl;
-        std::cin >> op;
-        std::vector<double> order;
-        string_parse(",", op, order);
-
-        machine.rows_multiplicate(a, order); machine.rows_multiplicate(b, order);
-       
-        nSTATE = "DRAW";
-        STATE = "TRANS";
-    } else if (input == "s"){
+    if (input == "s"){
 
         std::cout << "swapping ROW_A and ROW_B [Input: ROWA,ROWB]" << std::endl;
         std::cin >> op;
@@ -102,29 +98,81 @@ void operation(){
 
         nSTATE = "DRAW";
         STATE = "TRANS";
+    } else if (input == "a"){
+        if (difficulty>=1){
+            std::cout << "adding ROW_A to ROW_B [Input: ROWB,ROWA]" << std::endl;
+            std::cin >> op;
+            std::vector<double> order; // TODO change so that is does not have to be re-created all the time
+            string_parse(",", op, order);
+
+            machine.rows_add(a, order); machine.rows_add(b, order);
+            
+            nSTATE = "DRAW";
+            STATE = "TRANS";
+        } else {
+            std::cout << "Something was wrong with your input. Try again" << std::endl;
+            nSTATE = "DRAW";
+            STATE = "TRANS";
+          }
     } else if (input == "su"){
-        std::cout << "subtracting ROW_A from ROW_B [Input: ROWB,ROWA]" << std::endl;
-        std::cin >> op;
-        std::vector<double> order;
-        string_parse(",", op, order);
+        if (difficulty>=1){
+            std::cout << "subtracting ROW_A from ROW_B [Input: ROWB,ROWA]" << std::endl;
+            std::cin >> op;
+            std::vector<double> order;
+            string_parse(",", op, order);
 
-        machine.rows_subtract(a, order); machine.rows_subtract(b, order);
+            machine.rows_subtract(a, order); machine.rows_subtract(b, order);
+            
+            nSTATE = "DRAW";
+            STATE = "TRANS";
+        } else {
+            std::cout << "Something was wrong with your input. Try again" << std::endl;
+            nSTATE = "DRAW";
+            STATE = "TRANS";
+          }
+    } else if (input == "m"){
+        if (difficulty==2){
+            std::cout << "multiplicating ROW with factor [Input: ROW,FACTOR]" << std::endl;
+            std::cin >> op;
+            std::vector<double> order;
+            string_parse(",", op, order);
+
+            machine.rows_multiplicate(a, order); machine.rows_multiplicate(b, order);
         
-        nSTATE = "DRAW";
-        STATE = "TRANS";
+            nSTATE = "DRAW";
+            STATE = "TRANS";
+        } else {
+            std::cout << "Something was wrong with your input. Try again" << std::endl;
+            nSTATE = "DRAW";
+            STATE = "TRANS";
+          }
     } else if (input == "d"){
-        std::cout << "dividing ROW by divisor [Input: ROW,DIVISOR]" << std::endl;
-        std::cin >> op;
-        std::vector<double> order;
-        string_parse(",", op, order);
+        if (difficulty==2){
+            std::cout << "dividing ROW by divisor [Input: ROW,DIVISOR]" << std::endl;
+            std::cin >> op;
+            std::vector<double> order;
+            string_parse(",", op, order);
 
-        machine.rows_divide(a, order); machine.rows_divide(b, order);
-       
-        nSTATE = "DRAW";
-        STATE = "TRANS";
+            machine.rows_divide(a, order); machine.rows_divide(b, order);
+        
+            nSTATE = "DRAW";
+            STATE = "TRANS";
+        } else {
+            std::cout << "Something was wrong with your input. Try again" << std::endl;
+            nSTATE = "DRAW";
+            STATE = "TRANS";
+          }
     } else if (input == "e"){
         nSTATE = "EXIT";
         STATE = "TRANS";
+    } else if (input == "p"){ //   NEEDS TO BE REMOVED LATER
+        if (difficulty<2){
+          difficulty++;
+        }
+    } else if (input == "o"){ //   NEEDS TO BE REMOVED LATER
+        if (difficulty>0){
+          difficulty--;
+        }
     }
     
     
@@ -145,7 +193,9 @@ int main(){
 while (true)
 {
     if (STATE == "INIT"){
-    
+    // Setting initial difficulty level
+    difficulty = 0;
+
     // getting dimensions
     vars=2; eqs=2;
     machine.update_dimensions(eqs, vars);
